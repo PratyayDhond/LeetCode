e93a51b8f991c20cae7cd4da45290ffe98ed926d
Pratyay Dhond <dhondpratyay@gmail.com>
2021-07-22 19:13:30 +0530

    Add files via upload

---
+++ Arrays 101/Check If N and Its Double Exist - Approach2.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Place your class here
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+      sort(arr.begin(),arr.end());
+	  	for(int i = 0;i<arr.size();i++){
+	  		int n = arr[i];
+	  		if(n%2 == 0)
+	  		if(binary_search(arr.begin(),arr.end(),n/2)){
+	  			if(n == 0){
+	  				int count = 0;
+	  				for(auto a : arr){
+	  					if(a==n)
+	  						count++;
+					  }
+					  if(count>1)
+					  	return true;
+				  }else
+	  			return true;
+			  }
+		  }
+	  return false;	
+	}
+};
+
+int main(){
+	Solution s1;
+
+	//edit your function name here;
+	vector<int> arr = {0,0};
+	cout<< s1.checkIfExist(arr);
+	return 0;
+}
---
+++ Arrays 101/Check If N and Its Double Exist.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Place your class here
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+      
+	  for(int i = 0;i<arr.size()-1;i++){
+	  	for(int j = i+1;j<arr.size();j++){
+	  		if(arr[i] == arr[j] *2){
+	  			return true;
+			  }
+		  }
+		for(int j = i-1;j>=0;j--){
+			if(arr[i] == arr[j]*2){
+				return true;
+			}
+		}  
+	  }  
+     return false;         
+    }
+};
+
+int main(){
+	Solution s1;
+
+	//edit your function name here;
+	vector<int> arr = {7,1,14,11};
+	cout<< s1.checkIfExist(arr);
+	return 0;
+}
